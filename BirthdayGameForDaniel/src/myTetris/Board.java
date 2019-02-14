1df654fb8d29206090d005503e2a3e08fe4cbbe1
Angi <nailmaker.gigi@gmail.com>
2019-02-14 13:02:46 +0100

    Teljes kód, a mozgatás még nem 100%

--- BirthdayGameForDaniel/src/myTetris/Board.java
+++ BirthdayGameForDaniel/src/myTetris/Board.java
@@ -5,22 +5,23 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import javax.swing.Timer;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import myTetris.Shape.Tetris;
 
 public class Board extends JPanel implements ActionListener {
-    
+
     private static final int BOARD_WIDTH = 10;
     private static final int BOARD_HEIGHT = 22;
-    private static final Color[] COLORS = {
-        new Color(0, 0, 0), new Color(204, 102, 102),
-        new Color(102, 204, 102), new Color(102, 102, 104),
-        new Color(204, 204, 102), new Color(204, 102, 204),
-        new Color(204, 204, 102), new Color(204, 102, 204),
-        new Color(102, 204, 204), new Color(218, 170, 0)};
-    
+    private static final Color[] COLORS = {new Color(0, 0, 0),
+        new Color(204, 102, 102), new Color(102, 204, 102),
+        new Color(102, 102, 204), new Color(204, 204, 102),
+        new Color(204, 102, 204), new Color(102, 204, 204),
+        new Color(218, 170, 0)};
+
     private Timer timer;
     private boolean isFallingFinished = false;
     private boolean isStarted = false;
@@ -31,7 +32,7 @@ public class Board extends JPanel implements ActionListener {
     private JLabel stausBar;
     private Shape curPiece;
     private Tetris[] board;
-    
+
     public Board(BirthdayTertis parent) {
         setFocusable(true);
         curPiece = new Shape();
@@ -39,45 +40,46 @@ public class Board extends JPanel implements ActionListener {
         stausBar = parent.getStatusBar();
         board = new Tetris[BOARD_WIDTH * BOARD_HEIGHT];
         clearBoard();
+        addKeyListener(new MyTTetrisAdapter());
     }
-    
+
     public int squareHeight() {
-        return (int) getSize().getHeight() / BOARD_WIDTH;
+        return (int) getSize().getHeight() / BOARD_HEIGHT;
     }
-    
+
     public int squareWidth() {
-        return (int) getSize().getWidth() / BOARD_HEIGHT;
+        return (int) getSize().getWidth() / BOARD_WIDTH;
     }
-    
+
     public Tetris shapeAt(int x, int y) {
         return board[y * BOARD_WIDTH + x];
     }
-    
-    public void clearBoard() {
-        for (int i = 0; i < BOARD_WIDTH * BOARD_HEIGHT; i++) {
+
+    private void clearBoard() {
+        for (int i = 0; i < BOARD_HEIGHT * BOARD_WIDTH; i++) {
             board[i] = Tetris.NoShape;
         }
     }
-    
+
     private void pieceDropped() {
         for (int i = 0; i < 4; i++) {
             int x = curX + curPiece.x(i);
-            int y = curY + curPiece.y(i);
+            int y = curY - curPiece.y(i);
             board[y * BOARD_WIDTH + x] = curPiece.getShape();
         }
-        
+
         removeFullLines();
-        
+
         if (!isFallingFinished) {
             newPiece();
         }
     }
-    
+
     public void newPiece() {
         curPiece.setRandomShape();
         curX = BOARD_WIDTH / 2 + 1;
         curY = BOARD_HEIGHT - 1 + curPiece.minY();
-        
+
         if (!tryMove(curPiece, curX, curY - 1)) {
             curPiece.setShape(Tetris.NoShape);
             timer.stop();
@@ -85,13 +87,13 @@ public class Board extends JPanel implements ActionListener {
             stausBar.setText("Game Over");
         }
     }
-    
+
     private void oneLineDown() {
         if (!tryMove(curPiece, curX, curY - 1)) {
             pieceDropped();
         }
     }
-    
+
     @Override
     public void actionPerformed(ActionEvent ae) {
         if (isFallingFinished) {
@@ -109,9 +111,9 @@ public class Board extends JPanel implements ActionListener {
         g.fillRect(x + 1, y + 1, squareWidth() - 2, squareHeight() - 2);
         g.setColor(color.brighter());
         g.drawLine(x, y + squareHeight() - 1, x, y);
-        g.drawLine(x, y, squareHeight() - 1, y);
+        g.drawLine(x, y, x + squareHeight() - 1, y);
         g.setColor(color.darker());
-        g.drawLine(x + 1, y + squareHeight() - 1, squareWidth() - 1, y + squareHeight() - 1);
+        g.drawLine(x + 1, y + squareHeight() - 1, x + squareWidth() - 1, y + squareHeight() - 1);
         g.drawLine(x + squareWidth() - 1, y + squareHeight() - 1, x + squareWidth() - 1, y + 1);
     }
 
@@ -120,32 +122,32 @@ public class Board extends JPanel implements ActionListener {
     public void paint(Graphics g) {
         super.paint(g);
         Dimension size = getSize();
-        
+
         int boardTop = (int) size.getHeight() - BOARD_HEIGHT * squareHeight();
-        
+
         for (int i = 0; i < BOARD_HEIGHT; i++) {
             for (int j = 0; j < BOARD_WIDTH; ++j) {
                 Tetris shape = shapeAt(j, BOARD_HEIGHT - i - 1);
-                
+
                 if (shape != Tetris.NoShape) {
                     drawSquare(g, j * squareWidth(), boardTop + i * squareHeight(), shape);
                 }
             }
         }
         if (curPiece.getShape() != Tetris.NoShape) {
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < 4; ++i) {
                 int x = curX + curPiece.x(i);
                 int y = curY + curPiece.y(i);
                 drawSquare(g, x * squareWidth(), boardTop + (BOARD_HEIGHT - y - 1) * squareHeight(), curPiece.getShape());
             }
         }
     }
-    
+
     public void start() {
         if (isPaused) {
             return;
         }
-        
+
         isStarted = true;
         isFallingFinished = false;
         numLinesRemoved = 0;
@@ -153,69 +155,69 @@ public class Board extends JPanel implements ActionListener {
         newPiece();
         timer.start(); //ez indítja a timert, 400 ms közökkel
     }
-    
+
     public void pause() {
-        if (!isStarted) 
+        if (!isStarted) {
             return;
-        
-            isPaused = !isPaused;
-            
-            if (isPaused) {
-                timer.stop();
-                stausBar.setText("Paused");                
-            } else {
-                timer.start();
-                stausBar.setText(String.valueOf(numLinesRemoved));
-            }            
-            repaint();
         }
-    
-    
+
+        isPaused = !isPaused;
+
+        if (isPaused) {
+            timer.stop();
+            stausBar.setText("Paused");
+        } else {
+            timer.start();
+            stausBar.setText(String.valueOf(numLinesRemoved));
+        }
+        repaint();
+    }
+
     private boolean tryMove(Shape newPiece, int newX, int newY) {
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 4; ++i) {
             int x = newX + newPiece.x(i);
-            int y = newY + newPiece.y(i);
-            
+            int y = newY - newPiece.y(i);
+
             if (x < 0 || x >= BOARD_WIDTH || y < 0 || y >= BOARD_HEIGHT) {
                 return false;
             }
-            
+
             if (shapeAt(x, y) != Tetris.NoShape) {
                 return false;
             }
         }
-        
+
         curPiece = newPiece;
         curX = newX;
         curY = newY;
         repaint();
-        
+
         return true;
     }
-    
+
     private void removeFullLines() {
         int numFullLines = 0;
-        
+
         for (int i = BOARD_HEIGHT - 1; i >= 0; --i) {
             boolean lineIsFull = true;
-            
+
             for (int j = 0; j < BOARD_WIDTH; ++j) {
                 if (shapeAt(j, i) == Tetris.NoShape) {
                     lineIsFull = false;
                     break;
                 }
             }
-            
+
             if (lineIsFull) {
                 ++numFullLines;
-                
+
                 for (int k = i; k < BOARD_HEIGHT - 1; ++k) {
                     for (int j = 0; j < BOARD_WIDTH; ++j) {
                         board[k * BOARD_WIDTH + j] = shapeAt(j, k + 1);
                     }
                 }
             }
-            
+
             if (numFullLines > 0) {
                 numLinesRemoved += numFullLines;
                 stausBar.setText(String.valueOf(numLinesRemoved));
@@ -223,23 +225,64 @@ public class Board extends JPanel implements ActionListener {
                 curPiece.setShape(Tetris.NoShape);
                 repaint();
             }
-            
         }
     }
 
     //drop dovn függvény hozzáadás
     private void dropDown() {
         int newY = curY;
-        
+
         while (newY > 0) {
             if (!tryMove(curPiece, curX, newY - 1)) {
                 break;
             }
-            
             --newY;
         }
-        
+
         pieceDropped();
     }
-    
+
+    class MyTTetrisAdapter extends KeyAdapter {
+
+        @Override
+        public void keyPressed(KeyEvent ke) {
+            if (!isStarted || curPiece.getShape() == Tetris.NoShape) {
+                return;
+            }
+
+            int keyCode = ke.getKeyCode();
+            
+            if(keyCode == 'p' || keyCode == 'P')
+            pause();
+
+            if (isPaused) {
+                return;
+            }
+
+            switch (keyCode) {
+                case KeyEvent.VK_LEFT:
+                    tryMove(curPiece, curX - 1, curY);
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    tryMove(curPiece, curX + 1, curY);
+                    break;
+                case KeyEvent.VK_DOWN:
+                    tryMove(curPiece.rotateRight(), curX, curY);
+                    break;
+                case KeyEvent.VK_UP:
+                    tryMove(curPiece.rotateLeft(), curX, curY);
+                    break;
+                case KeyEvent.VK_SPACE:
+                    dropDown();
+                    break;
+                case 'd':
+                    oneLineDown();
+                    break;
+                case 'D':
+                    oneLineDown();
+                    break;
+            }
+        }
+    }
+
 }
